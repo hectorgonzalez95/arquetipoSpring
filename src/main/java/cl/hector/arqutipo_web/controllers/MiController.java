15348f46b1743a4999517054f1983ff50bf84511
Hector <hector.gonzalezp@mineduc.cl>
2020-12-23 11:12:21 -0300

    Se agrega miController

---
+++ src/main/java/cl/hector/arqutipo_web/controllers/MiController.java
@@ -0,0 +1,19 @@
+package cl.hector.arqutipo_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("mi")
+@Slf4j
+public class MiController {
+
+	@GetMapping(value = "mi2")
+	public ModelAndView prueba(){		
+		return new ModelAndView();	
+	}
+}
